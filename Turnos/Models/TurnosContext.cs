a4617bce363b6d48cd6938493bba9ed53333e87b
Estiben Montoya Taborda <estiben.montoya@cidet.org.co>
2023-10-27 06:52:25 -0500

    descripcion entidad especialidad

--- Turnos/Models/TurnosContext.cs
+++ Turnos/Models/TurnosContext.cs
@@ -5,11 +5,24 @@ namespace Turnos.Models
     public class TurnosContext:DbContext
     {
         public TurnosContext(DbContextOptions<TurnosContext> options)
-            : base(options)
+        : base(options)
         { 
         
         }
 
         public DbSet<Especialidad> Especialidades { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<Especialidad>(entidad =>
+            {
+                entidad.ToTable("Especialidades");
+                entidad.HasKey(e=>e.Id);
+                entidad.Property(e => e.Descripcion)
+                .IsRequired()
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            });
+        }
     }
 }
